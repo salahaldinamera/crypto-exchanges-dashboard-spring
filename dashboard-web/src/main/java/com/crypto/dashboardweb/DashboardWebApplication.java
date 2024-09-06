2f46cffe25dd95364709b2917990475787ba9e88
Salah-Aldin Amera <salah.aldin.dev@gmail.com>
2024-09-06 23:47:21 +0000

    init-project

---
+++ dashboard-web/src/main/java/com/crypto/dashboardweb/DashboardWebApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.dashboardweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardWebApplication.class, args);
+    }
+
+}
---
+++ dashboard-web/src/test/java/com/crypto/dashboardweb/DashboardWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.dashboardweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
