3c8263f79ad76ce72318e95e13e5634bd3fbbf87
Salah-Aldin Amera <salah.aldin.dev@gmail.com>
2024-09-18 19:31:30 +0000

    init-xchange-service-module

---
+++ xchange-service/src/main/java/com/crypto/xchangeservice/XchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.xchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XchangeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XchangeServiceApplication.class, args);
+    }
+
+}
---
+++ xchange-service/src/test/java/com/crypto/xchangeservice/XchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crypto.xchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
